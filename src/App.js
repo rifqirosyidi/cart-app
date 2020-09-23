e7202944245e6b24ae47d74cc94711c4fbc20e99
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-09-23 16:07:19 +0700

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Feature 1 Test
 import React, { Component } from "react";
 import data from "./data.json";
+import Filter from "./components/Filter";
 import Products from "./components/Products";
 import Navbar from "./components/Navbar";
 
@@ -9,10 +10,48 @@ class App extends Component {
     super();
     this.state = {
       products: data.products,
-      size: "",
+      ram: "",
       sort: "",
     };
   }
+
+  sortProducts = (e) => {
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a.id > b.id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterRam = (e) => {
+    if (e.target.value === "") {
+      this.setState({
+        ram: e.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        ram: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableRam.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
 
   render() {
     return (
@@ -24,6 +63,13 @@ class App extends Component {
         <main className="mx-2 ">
           <div className="row">
             <div className="col l8 m8 s12">
+              <Filter
+                count={this.state.products.length}
+                ram={this.state.ram}
+                sort={this.state.sort}
+                filterRam={this.filterRam}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
             <div className="col l4 m4 s12 my-2 sidebar">Sidebar</div>
